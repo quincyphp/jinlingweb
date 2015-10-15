eea60933defa42edfccf4d792ca2e895f71e6b92
quincyphp <quincyphp@gmail.com>
2015-10-15 22:40:53 +0800

    修改注释

--- index.php
+++ index.php
@@ -17,13 +17,13 @@ $new_info = $mysql->get_all('select * from `article`');
 $link = $mysql->get_all('select * from `friendlink`');
 
 
-#对内容进行赋值
+# 对内容进行赋值
 $smarty->assign('banner_info',$banner_info);
 $smarty->assign('about_info',$about_info);
 $smarty->assign('pro_info',$pro_info);
 $smarty->assign('new_info',$new_info);
 $smarty->assign('link',$link);
 
-#显示模板文件
+# 显示模板文件
 $smarty->display('index.html');
 ?>
